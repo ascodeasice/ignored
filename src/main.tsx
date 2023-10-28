1e61bba6623ebd27073c56024386775710d58984
Leo Wang <ab0988956087@gmail.com>
2023-10-29 01:29:49 +0800

    feat(app): update font

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import '@fontsource/lato/400.css'
+import '@fontsource/fira-mono'
+import theme from './theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Fira Mono', sans-serif`,
+    text: `'Lato', sans-serif`,
+  },
+})
+
+export default theme
